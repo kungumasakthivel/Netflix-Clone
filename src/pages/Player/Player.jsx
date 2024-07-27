6dcefc92f6dacf265c93ad274b1144620dc9a30e
kungumasakthivel <kungumasakthivel.g2020aids@sece.ac.in>
2024-07-27 22:24:48 +0530

    navbar part1 done

---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Player.css'
+
+const Player = () => {
+  return (
+    <div className='player'>
+      
+    </div>
+    import './Player.css'
+  )
+}
+
+export default Player className='player'
