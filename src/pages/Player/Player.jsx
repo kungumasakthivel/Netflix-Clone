44517c77c01c047972f7d89ba77db57c8e1f164f
kungumasakthivel <kungumasakthivel.k@gmail.com>
2024-07-29 11:16:21 +0530

    api done for video fetching

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams();
+
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  })
+
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwY2I5MTQ0NzkxY2VhZjZjZDQ4NDg1ODNjOTNhMTI0MSIsIm5iZiI6MTcyMjE3Mjc4NC45Nzg3Niwic3ViIjoiNjZhNjQ0NDE4M2U4NTU3YzBmNDBiYTU3Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.DO56RggNUPiAaon8kbr0YgPjm_4HD7klclvDX0thWO4'
+    }
+  };
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+
+  }, [])
+
+  
+
+
   return (
     <div className='player'>
-      
+      <img src={back_arrow_icon} alt="" onClick={() => {navigate(-2)}} />
+      <iframe width='90%' height='90%'
+      src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer'
+      frameBorder='0'
+      allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
-    import './Player.css'
   )
 }
 
-export default Player className='player'
+export default Player
